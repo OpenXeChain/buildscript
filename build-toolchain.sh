43161098d4e60b1777f5e4e4bef7e66553baf9a7
Aiden Isik <aidenisik+git@member.fsf.org>
2025-07-09 00:22:58 +0100

    Initial commit (compiler-rt builtins don't get built...)

---
+++ build-toolchain.sh
@@ -0,0 +1,215 @@
+#!/usr/bin/env bash
+
+# Build script for the OpenXeChain toolchain project
+#
+# Copyright (c) 2025 Aiden Isik
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# ANSI colour escape codes
+ANSI_RED="\033[31m"
+ANSI_GRN="\033[32m"
+ANSI_CLR="\033[0m"
+
+# Toolchain name
+TOOLCHAIN_NAME="OpenXeChain"
+TOOLCHAIN_STEM="${ANSI_GRN}${TOOLCHAIN_NAME}${ANSI_CLR}> "
+
+# User-configurable variables
+PREFIX="$(realpath ${PREFIX:-sysroot})" # Sysroot for the toolchain to be installed into
+HOST_CC="${HOST_CC:-clang}" # Host compiler to use (MUST BE CLANG)
+HOST_CXX="${HOST_CXX:-clang++}"
+BUILD_TYPE="${BUILD_TYPE:-Release}" # Debug level to build LLVM in
+PARALLEL="${PARALLEL:-$(nproc)}" # Number of parallel make jobs to run
+
+# Static variables
+LLVM_TARGET="ppc32-xbox360"
+NEWLIB_TARGET="ppc-xbox360"
+OLD_PWD="${PWD}"
+
+# Build log path
+BUILD_LOG="${OLD_PWD}/build.log"
+echo "" > "${BUILD_LOG}" # Delete the old logs, if they exist
+
+# If we fail to build, run this
+fail_build()
+{
+    echo -e "${TOOLCHAIN_STEM}${ANSI_RED}Failed to build! Check build.log.${ANSI_CLR}"
+    exit 1
+}
+
+cd "$(dirname $0)" || fail_build
+
+if [[ ! -d "newlib" || ! -d "llvm" || ! -d "synthxex" || ! -d "xecorelib" ]]; then
+    echo -e "${TOOLCHAIN_STEM}${ANSI_RED}Submodules are missing! Please re-clone this repository with --recursive.${ANSI_CLR}"
+    fail_build
+fi
+
+# Create the sysroot directory, if it doesn't already exist
+echo -e "${TOOLCHAIN_STEM}Creating sysroot directory \"${PREFIX}\"."
+mkdir -pv "${PREFIX}" >> "${BUILD_LOG}" 2>&1 || fail_build
+
+# Build the LLVM cross compiler
+echo -e "${TOOLCHAIN_STEM}Getting ready to build the cross compiler."
+mkdir -pv "build" >> "${BUILD_LOG}" 2>&1 || fail_build
+cd "build" || fail_build
+
+# Configure it first
+echo -e "${TOOLCHAIN_STEM}Configuring the cross compiler... (this may take a while)"
+cmake -DCMAKE_C_COMPILER="${HOST_CC}" \
+      -DCMAKE_CXX_COMPILER="${HOST_CXX}" \
+      -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
+      -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+      -DLLVM_ENABLE_PROJECTS="lld;clang;compiler-rt" \
+      -DLLVM_TARGETS_TO_BUILD=PowerPC \
+      -DLLVM_DEFAULT_TARGET_TRIPLE="${LLVM_TARGET}" \
+      -DLLVM_INSTALL_BINUTILS_SYMLINKS=true \
+      -DLLVM_INSTALL_CCTOOLS_SYMLINKS=true \
+      -DLLVM_INSTALL_TOOLCHAIN_ONLY=true \
+      -G "Ninja" ../llvm/llvm >> "${BUILD_LOG}" 2>&1 || fail_build
+
+# Now build and install
+echo -e "${TOOLCHAIN_STEM}Building the cross compiler... (this may take a WHILE)"
+ninja -j${PARALLEL} >> "${BUILD_LOG}" 2>&1 || fail_build
+
+echo -e "${TOOLCHAIN_STEM}Installing the cross compiler... (this may take a while)"
+ninja install >> "${BUILD_LOG}" 2>&1 || fail_build
+
+echo -e "${TOOLCHAIN_STEM}Cross compiler built and installed!"
+rm -rf * # Clear the build directory, ready for xecorelib
+
+# Clear the environment variables the C/C++ compiler is sensitive to,
+# to avoid pollution.
+# We restore them later.
+OLD_LIBRARY_PATH="${LIBRARY_PATH}"
+OLD_C_INCLUDE_PATH="${C_INCLUDE_PATH}"
+OLD_CPLUS_INCLUDE_PATH="${CPLUS_INCLUDE_PATH}"
+export LIBRARY_PATH=""
+export C_INCLUDE_PATH=""
+export CPLUS_INCLUDE_PATH=""
+
+# Define the default command line flags to be used by the cross-compiler.
+# Linkage to Newlib/xecorelib is also added to these files, but after it's been built and installed.
+echo -e "${TOOLCHAIN_STEM}Writing initial Clang configuration scripts."
+
+cat > "${PREFIX}/bin/clang.cfg" << EOF
+--sysroot=<CFGDIR>/..
+--rtlib=compiler-rt
+-fdeclspec
+EOF
+
+cat > "${PREFIX}/bin/clang++.cfg" << EOF
+--sysroot=<CFGDIR>/..
+--rtlib=compiler-rt
+-fdeclspec
+EOF
+
+# Build xecorelib
+echo -e "${TOOLCHAIN_STEM}Building and installing xecorelib."
+
+# Run the xecorelib build script
+PREFIX="${PREFIX}" bash ../xecorelib/install.sh >> "${BUILD_LOG}" 2>&1 || fail_build
+
+# Also install to PWD, to build Newlib with it
+BINDIR="${PREFIX}/bin" PREFIX="${PWD}/xecorelibtmp" bash ../xecorelib/install.sh >> "${BUILD_LOG}" 2>&1 || fail_build
+echo -e "${TOOLCHAIN_STEM}Built and installed xecorelib!"
+
+# Clear the build directory, ready for Newlib
+ls | grep -P "^(?!xecorelibtmp$).*$" | xargs -d"\n" rm -rf
+
+# Build the Newlib libc
+echo -e "${TOOLCHAIN_STEM}Getting ready to build the Newlib C library."
+
+# Configure Newlib
+echo -e "${TOOLCHAIN_STEM}Configuring the Newlib C library... (this may take a while)"
+
+../newlib/newlib/configure \
+    CC="${PREFIX}/bin/clang -nostdlib -I${PWD}/xecorelibtmp/include" \
+    CPP="${PREFIX}/bin/clang-cpp" \
+    LD="${PREFIX}/bin/lld-link" \
+    AR="${PREFIX}/bin/llvm-ar" \
+    AS="${PREFIX}/bin/llvm-as" \
+    STRIP="${PREFIX}/bin/llvm-strip" \
+    RANLIB="${PREFIX}/bin/llvm-ranlib" \
+    --prefix="${PREFIX}" \
+    --host="${NEWLIB_TARGET}" \
+    --target="${NEWLIB_TARGET}" \
+    --enable-newlib-supplied-syscalls=yes \
+    --enable-newlib-mb \
+    --enable-newlib-iconv >> "${BUILD_LOG}" 2>&1 || fail_build
+
+# Now build and install
+echo -e "${TOOLCHAIN_STEM}Building the Newlib C library... (this may take a while)"
+make -j${PARALLEL} >> "${BUILD_LOG}" 2>&1 || fail_build
+
+echo -e "${TOOLCHAIN_STEM}Installing the Newlib C library... (this may take a while)"
+make install >> "${BUILD_LOG}" 2>&1 || fail_build
+
+# Restore the environment variables the C/C++ compiler is sensitive to
+export LIBRARY_PATH="${OLD_LIBRARY_PATH}"
+export C_INCLUDE_PATH="${OLD_C_INCLUDE_PATH}"
+export CPLUS_INCLUDE_PATH="${OLD_CPLUS_INCLUDE_PATH}"
+
+# Add Newlib/xecorelib linkage to the default compiler flags now that it is installed
+echo -e "${TOOLCHAIN_STEM}Adding Newlib linkage to Clang configuration scripts."
+
+cat >> "${PREFIX}/bin/clang.cfg" << EOF
+-isystem <CFGDIR>/../${NEWLIB_TARGET}/include
+-isystem <CFGDIR>/../include
+-Wl,/libpath:<CFGDIR>/../${NEWLIB_TARGET}/lib,/libpath:<CFGDIR>/../lib
+-Wl,/defaultlib:xecorelib.a,/defaultlib:libc.a
+EOF
+
+cat >> "${PREFIX}/bin/clang++.cfg" << EOF
+-isystem <CFGDIR>/../${NEWLIB_TARGET}/include
+-isystem <CFGDIR>/../include
+-Wl,/libpath:<CFGDIR>/../${NEWLIB_TARGET}/lib,/libpath:<CFGDIR>/../lib
+-Wl,/defaultlib:xecorelib.a,/defaultlib:libc.a
+EOF
+
+echo -e "${TOOLCHAIN_STEM}Newlib C library built and installed!"
+rm -rf * # Clear the build directory, ready for SynthXEX
+
+# Build SynthXEX
+echo -e "${TOOLCHAIN_STEM}Getting ready to build SynthXEX."
+
+# Configure SynthXEX
+echo -e "${TOOLCHAIN_STEM}Configuring SynthXEX..."
+
+cmake -DCMAKE_C_COMPILER="${HOST_CC}" \
+      -DCMAKE_CXX_COMPILER="${HOST_CXX}" \
+      -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
+      -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+      -G "Ninja" ../synthxex >> "${BUILD_LOG}" 2>&1 || fail_build
+
+# Now build and install
+echo -e "${TOOLCHAIN_STEM}Building SynthXEX..."
+ninja -j${PARALLEL} >> "${BUILD_LOG}" 2>&1 || fail_build
+
+echo -e "${TOOLCHAIN_STEM}Installing SynthXEX..."
+ninja install >> "${BUILD_LOG}" 2>&1 || fail_build
+
+echo -e "${TOOLCHAIN_STEM}SynthXEX built and installed!"
+
+# Finished
+echo -e "${TOOLCHAIN_STEM}${ANSI_GRN}${TOOLCHAIN_NAME} has been built successfully.${ANSI_CLR}"
+echo -e "${TOOLCHAIN_STEM}${ANSI_GRN}Current installled location: ${PREFIX}.${ANSI_CLR}"
+echo -e "${TOOLCHAIN_STEM}${ANSI_GRN}This toolchain is portable, it can be moved to any path and still work.${ANSI_CLR}"
+echo -e "${TOOLCHAIN_STEM}${ANSI_GRN}Ensure C_INCLUDE_PATH, CPLUS_INCLUDE_PATH, and LIBRARY_PATH${ANSI_CLR}"
+echo -e "${TOOLCHAIN_STEM}${ANSI_GRN}are set blank when using the toolchain, or you may encounter${ANSI_CLR}"
+echo -e "${TOOLCHAIN_STEM}${ANSI_GRN}interference from the host libraries.${ANSI_CLR}"
+
+cd ..
+rm -rf build
+#rm -f build.log
